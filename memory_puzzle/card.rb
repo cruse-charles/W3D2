d8401e3ebd0217e195b7f0ab98cd6c2b97e2ef83
Charles Cruse <charle9@vt.edu>
2023-01-24 13:51:27 -0500

    2nd review

---
+++ memory_puzzle/card.rb
@@ -0,0 +1,24 @@
+class Card
+    attr_reader :face_val, :state
+
+    def initialize(face_val)
+        @state = hidden
+        @face_val = face_val
+
+    end
+
+
+    def hide
+        self.state = hidden
+    end
+
+    def reveal
+        self.state = shown
+    end
+
+    def ==(other_card)
+        self.face_val == other_card.face_val
+    end
+
+
+end
